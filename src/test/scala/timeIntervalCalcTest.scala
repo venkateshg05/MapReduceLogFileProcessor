6d6ff9e39a53bb976e31f954aa47052615c5dda5
venkateshg05 <venkatesh_gopal@outlook.com>
2022-10-16 19:29:04 -0500

    updated conf file

--- src/test/scala/timeIntervalCalcTest.scala
+++ src/test/scala/timeIntervalCalcTest.scala
@@ -5,20 +5,30 @@ import java.time.{Duration, LocalTime}
 
 class timeIntervalCalcTest extends funsuite.AnyFunSuite {
   test("getTimeInterval") {
-    val ts = LocalTime.parse("12:59:00.000")
-    val st = LocalTime.parse(Parameters.startTime)
-    val interval = Parameters.timeInterval
-    val en = LocalTime.parse(Parameters.endTime)
+    val ts = LocalTime.parse("12:57:52.000")
+    val st = LocalTime.parse("12:00")
+    val interval = 5
+    val en = LocalTime.parse("14:30")
     val totalDuration = Duration.between(st, en).toMinutes()
     assert(
-      GetTimeInterval.getTimeInterval(st, 0, totalDuration.toInt/interval, ts, interval) == "12:59, 13:00"
+      GetTimeInterval.getTimeInterval(st, 0, totalDuration.toInt/interval, ts, interval) == "12:56, 13:00"
+    )
+  }
+  test("getTimeIntervalBoundaryCase") {
+    val ts = LocalTime.parse("12:50:00.000")
+    val st = LocalTime.parse("12:00")
+    val interval = 10
+    val en = LocalTime.parse("14:00")
+    val totalDuration = Duration.between(st, en).toMinutes()
+    assert(
+      GetTimeInterval.getTimeInterval(st, 0, totalDuration.toInt / interval, ts, interval) == "12:41, 12:50"
     )
   }
   test("getTimeIntervalOutOfBounds") {
     val ts = LocalTime.parse("11:59:00.000")
-    val st = LocalTime.parse(Parameters.startTime)
-    val interval = Parameters.timeInterval
-    val en = LocalTime.parse(Parameters.endTime)
+    val st = LocalTime.parse("12:00")
+    val interval = 5
+    val en = LocalTime.parse("14:00")
     val totalDuration = Duration.between(st, en).toMinutes()
     assert(
       GetTimeInterval.getTimeInterval(st, 0, totalDuration.toInt / interval, ts, interval) == "Time not within bounds"
